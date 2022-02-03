e43f7c0b16cf4b091f0db8d6c162c3fae9a9408c
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-02-03 15:20:09 +0530

    questions and search query done

--- app/src/main/java/com/example/leetdroid/ui/fragments/AllQuestionsFragment.kt
+++ app/src/main/java/com/example/leetdroid/ui/fragments/AllQuestionsFragment.kt
@@ -1,44 +1,40 @@
 package com.example.leetdroid.ui.fragments
 
+
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import androidx.core.os.bundleOf
+import androidx.core.widget.NestedScrollView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.leetdroid.R
 import com.example.leetdroid.adapter.AllQuestionsAdapter
+import com.example.leetdroid.api.LeetCodeRequests
 import com.example.leetdroid.api.URL
 import com.example.leetdroid.databinding.FragmentAllQuestionsBinding
+import com.example.leetdroid.extensions.closeKeyboard
+import com.example.leetdroid.extensions.showSnackBar
 import com.example.leetdroid.model.AllQuestionsModel
+import com.example.leetdroid.ui.base.BaseFragment
 import com.example.leetdroid.utils.JsonUtils
+import com.google.gson.Gson
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 import java.lang.String.*
-import androidx.core.widget.NestedScrollView
-import com.example.leetdroid.extensions.showSnackBar
-import com.example.leetdroid.api.LeetCodeRequests
-
-import com.example.leetdroid.data.entitiy.AllQuestions
-import com.example.leetdroid.ui.base.BaseFragment
-
-
-import com.google.gson.Gson
-
 import java.util.*
+
 
 class AllQuestionsFragment : BaseFragment(), AllQuestionsAdapter.OnItemClicked {
 
     private lateinit var fragmentAllQuestionsBinding: FragmentAllQuestionsBinding
     private lateinit var questionJson: AllQuestionsModel
-    private var allQuestionsAdapter = AllQuestionsAdapter()
-    var questionList = mutableListOf<AllQuestions>()
-    var arrJournals = ArrayList<AllQuestions>()
-
+    private lateinit var allQuestionsAdapter: AllQuestionsAdapter
+    private lateinit var noQuestionsView: View
+    private lateinit var generalErrorView: View
     private var limit = 10
 
     override fun onCreateView(
@@ -49,6 +45,9 @@ class AllQuestionsFragment : BaseFragment(), AllQuestionsAdapter.OnItemClicked {
         fragmentAllQuestionsBinding = FragmentAllQuestionsBinding.inflate(layoutInflater)
         val rootView = fragmentAllQuestionsBinding.root
 
+        noQuestionsView = rootView.findViewById(R.id.view_no_questions)
+        generalErrorView = rootView.findViewById(R.id.view_general_error)
+        allQuestionsAdapter = AllQuestionsAdapter(requireContext(), requireActivity())
         loadQuestionList(limit)
 
         //  adding pagination
@@ -65,11 +64,47 @@ class AllQuestionsFragment : BaseFragment(), AllQuestionsAdapter.OnItemClicked {
         return rootView
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
-    private fun loadQuestionList(limit: Int) {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // clear the Main Activity's menu
+        menu.clear()
+        inflater.inflate(R.menu.all_questions_list_menu, menu)
+
+        val searchItem = menu.findItem(R.id.search_bar)
+        val searchView: SearchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    val searchKeywords: String = query
+                    fragmentAllQuestionsBinding.allQuestionsNested.visibility = View.GONE
+                    searchQuestions(searchKeywords)
+                    fragmentAllQuestionsBinding.questionListProgressBar.visibility = View.VISIBLE
+                    closeKeyboard()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return false
+                }
+            })
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun questionListApiCall(limit: Int, filters: LeetCodeRequests.Filters): Call {
         val okHttpClient = OkHttpClient()
         val postBody: String =
-            Gson().toJson(LeetCodeRequests.Helper.getAllQuestionsRequest("", limit))
+            Gson().toJson(
+                LeetCodeRequests.Helper.getAllQuestionsRequest(
+                    "", limit,
+                    filters
+                )
+            )
         val requestBody: RequestBody =
             postBody.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
         val headers: Headers = Headers.Builder()
@@ -80,30 +115,79 @@ class AllQuestionsFragment : BaseFragment(), AllQuestionsAdapter.OnItemClicked {
             .post(requestBody)
             .url(URL.graphql)
             .build()
-        val call: Call = okHttpClient.newCall(request)
+        return okHttpClient.newCall(request)
+    }
+
+    private fun loadQuestionList(limit: Int) {
+
+        val call: Call =
+            questionListApiCall(limit, filters = LeetCodeRequests.Filters(tags = listOf()))
         call.enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                //TODO add try catch block on every json thing, not sure why but gives error, maybe show somethign went wrong please try again
+                try {
+                    questionJson = JsonUtils.generateObjectFromJson(
+                        response.body!!.string(),
+                        AllQuestionsModel::class.java
+                    )
+                    activity?.runOnUiThread {
+                        allQuestionsAdapter.setData(questionJson)
+                        fragmentAllQuestionsBinding.allQuestionsRecyclerView.layoutManager =
+                            LinearLayoutManager(context)
+                        fragmentAllQuestionsBinding.allQuestionsRecyclerView.adapter =
+                            allQuestionsAdapter
+
+                        allQuestionsAdapter.setOnClick(this@AllQuestionsFragment)
+                        checkIfEmpty()
+
+                    }
+                } catch (exception: Exception) {
+                    generalErrorView.visibility = View.VISIBLE
+                }
+            }
 
             override fun onFailure(call: Call, e: IOException) {
                 showSnackBar(requireActivity(), e.message)
                 Log.d(Constant.TAG, call.toString(), e)
             }
 
+        })
+    }
+
+    private fun searchQuestions(query: String) {
+        val call: Call =
+            questionListApiCall(limit, LeetCodeRequests.Filters(searchKeywords = query))
+        call.enqueue(object : Callback {
+
             override fun onResponse(call: Call, response: Response) {
                 questionJson = JsonUtils.generateObjectFromJson(
                     response.body!!.string(),
                     AllQuestionsModel::class.java
                 )
-                activity?.runOnUiThread {
-                    allQuestionsAdapter.setData(questionJson)
-                    fragmentAllQuestionsBinding.allQuestionsRecyclerView.layoutManager =
-                        LinearLayoutManager(context)
-                    fragmentAllQuestionsBinding.allQuestionsRecyclerView.adapter =
-                        allQuestionsAdapter
 
-                    allQuestionsAdapter.setOnClick(this@AllQuestionsFragment)
-                    checkIfEmpty()
+                if (questionJson.data?.problemsetQuestionList != null) {
+                    activity?.runOnUiThread {
+                        allQuestionsAdapter.setData(questionJson)
+                        fragmentAllQuestionsBinding.allQuestionsRecyclerView.layoutManager =
+                            LinearLayoutManager(context)
+                        fragmentAllQuestionsBinding.allQuestionsRecyclerView.adapter =
+                            allQuestionsAdapter
 
+                        allQuestionsAdapter.setOnClick(this@AllQuestionsFragment)
+                        checkIfEmpty()
+                    }
+                } else {
+                    activity?.runOnUiThread {
+                        fragmentAllQuestionsBinding.questionListProgressBar.visibility = View.GONE
+                        noQuestionsView.visibility = View.VISIBLE
+                    }
                 }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                showSnackBar(requireActivity(), e.message)
+                Log.d(Constant.TAG, call.toString(), e)
             }
         })
     }
--- app/src/main/java/com/example/leetdroid/utils/CommonFunctions.kt
+++ app/src/main/java/com/example/leetdroid/utils/CommonFunctions.kt
@@ -1,6 +1,10 @@
 package com.example.leetdroid.utils
 
+import android.R
 import android.app.Activity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import com.example.leetdroid.extensions.openActivity
 import com.example.leetdroid.ui.authentication.LoginActivity
 import com.example.leetdroid.utils.dialog.AlertDialogShower
@@ -8,6 +12,7 @@ import com.example.leetdroid.utils.dialog.AppDialogs
 import com.google.firebase.auth.FirebaseAuth
 import kotlin.math.pow
 import kotlin.math.roundToInt
+
 
 class CommonFunctions {
     object Logout {
@@ -23,9 +28,18 @@ class CommonFunctions {
     }
 
     object Round {
-        private fun roundDouble(value: Double, precision: Int): Double {
+        fun roundDouble(value: Double, precision: Int): Double {
             val scale = 10.0.pow(precision.toDouble()).toInt()
             return (value * scale).roundToInt().toDouble() / scale
+        }
+    }
+
+    object MenuItemVisibility {
+        fun setItemsVisibility(menu: Menu, exception: MenuItem, visible: Boolean) {
+            for (i in 0 until menu.size()) {
+                val item = menu.getItem(i)
+                if (item !== exception) item.isVisible = visible
+            }
         }
     }
 }
