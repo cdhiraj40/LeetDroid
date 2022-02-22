947932146b279cae8ceca35d8e62de83ef112f64
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-02-23 03:30:10 +0530

    added about page, licenses

---
+++ app/src/main/java/com/example/leetdroid/model/LicensesModel.kt
@@ -0,0 +1,28 @@
+package com.example.leetdroid.model
+
+import java.io.Serializable
+
+/**
+ *  Data Model for the all questions list' error
+ *  if no data comes we check for error here
+ */
+class LicensesModel : Serializable {
+
+    /**
+     *
+     * { "libraries": { "library": [ { "name": "LeetDroid", "author": "Dhiraj Chauhan", "website": "https://github.com/cdhiraj40/LeetDroid/", "license": "MIT License" },
+     * { "name": "Android Jetpack", "author": "Google", "website": "https://developer.android.com/jetpack", "license": "Apache 2.0 License" },
+     */
+    var libraries: LibrariesNode? = null
+
+    class LibrariesNode {
+        var library: List<Library>? = null
+
+        class Library {
+            var name: String? = null
+            var author: String? = null
+            var website: String? = null
+            var license: String? = null
+        }
+    }
+}
