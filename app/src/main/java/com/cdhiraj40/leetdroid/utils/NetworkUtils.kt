b7a686f9489a65cf1ddff535062d9b4eea138b40
bissu <bismeetsingh352@gmail.com>
2022-10-23 09:02:33 +0530

    Show progress bar if missing internet connection. Use api specific checks rather than callbacks.

--- app/src/main/java/com/cdhiraj40/leetdroid/utils/NetworkUtils.kt
+++ app/src/main/java/com/cdhiraj40/leetdroid/utils/NetworkUtils.kt
@@ -2,27 +2,33 @@ package com.cdhiraj40.leetdroid.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 
-object NetworkUtils {
-    /**
-     * check availability of any network
-     *
-     * @return true if a network is ready to be used
-     */
-    fun isNetworkAvailable(context: Context): Boolean {
-        val connectivity = context
-            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfos = connectivity.allNetworkInfo
-        for (networkInfo in networkInfos) {
-            if (isNetworkConnectionOK(networkInfo)) {
-                return true
-            }
-        }
-        return false
+fun Context.hasNetwork(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        checkConnected(connectivityManager)
+    } else {
+        checkConnectedLegacy(connectivityManager)
     }
+}
 
-    private fun isNetworkConnectionOK(networkInfo: NetworkInfo): Boolean {
-        return networkInfo.state == NetworkInfo.State.CONNECTED
-    }
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun checkConnected(connectivityManager: ConnectivityManager): Boolean {
+    val activeNetwork = connectivityManager.activeNetwork
+    activeNetwork ?: return false
+    val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+    capabilities ?: return false
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || capabilities.hasTransport(
+        NetworkCapabilities.TRANSPORT_WIFI
+    )
+}
+
+fun checkConnectedLegacy(connectivityManager: ConnectivityManager): Boolean {
+    val networkInfo = connectivityManager.activeNetworkInfo
+    networkInfo ?: return false
+    return networkInfo.isConnected && (networkInfo.type == ConnectivityManager.TYPE_WIFI || networkInfo.type == ConnectivityManager.TYPE_MOBILE)
 }
