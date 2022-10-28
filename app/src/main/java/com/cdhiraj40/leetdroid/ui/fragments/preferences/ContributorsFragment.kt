74b32a71c9fb2e60423e386996453ac0fad2d6ac
mksiddiq <mksiddiq99100@gmail.com>
2022-10-28 23:59:29 +0530

    Added internet network connection alert dialogue box

--- app/src/main/java/com/cdhiraj40/leetdroid/ui/fragments/preferences/ContributorsFragment.kt
+++ app/src/main/java/com/cdhiraj40/leetdroid/ui/fragments/preferences/ContributorsFragment.kt
@@ -1,10 +1,15 @@
 package com.cdhiraj40.leetdroid.ui.fragments.preferences
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cdhiraj40.leetdroid.R
@@ -14,6 +19,8 @@ import com.cdhiraj40.leetdroid.databinding.FragmentContributorsBinding
 import com.cdhiraj40.leetdroid.model.ContributorListModel
 import com.cdhiraj40.leetdroid.utils.CommonUtils.openLink
 import com.cdhiraj40.leetdroid.utils.Constant
+import com.cdhiraj40.leetdroid.utils.checkConnected
+import com.cdhiraj40.leetdroid.utils.checkConnectivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -45,35 +52,52 @@ class ContributorsFragment : Fragment(), ContributorListAdapter.ContributorClick
 
     private fun getContributors(
     ) {
-        val apiInterface =
-            GithubApi.create().getContributors(Constant().userName, Constant().repositoryName)
+        if(checkConnectivity(activity as Context)){
+            val apiInterface =
+                GithubApi.create().getContributors(Constant().userName, Constant().repositoryName)
 
-        apiInterface.enqueue(object : Callback<ContributorListModel> {
-            override fun onResponse(
-                Call: Call<ContributorListModel>?,
-                response: Response<ContributorListModel>?
-            ) {
-                if (response?.body() != null) {
-                    val body = response.body()!!
-                    Log.d(
-                        Constant.TAG(ContributorsFragment::class.java).toString(), body.toString()
-                    )
-                    setUpRecyclerView(body)
-                    loadingView.visibility = View.GONE
-                    contributorsBinding.contributorRecyclerView.visibility=View.VISIBLE;
+            apiInterface.enqueue(object : Callback<ContributorListModel> {
+                override fun onResponse(
+                    Call: Call<ContributorListModel>?,
+                    response: Response<ContributorListModel>?
+                ) {
+                    if (response?.body() != null) {
+                        val body = response.body()!!
+                        Log.d(
+                            Constant.TAG(ContributorsFragment::class.java).toString(), body.toString()
+                        )
+                        setUpRecyclerView(body)
+                        loadingView.visibility = View.GONE
+                        contributorsBinding.contributorRecyclerView.visibility=View.VISIBLE;
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<ContributorListModel>?, throwable: Throwable) {
-                Log.d(
-                    Constant.TAG(ContributorsFragment::class.java).toString(),
-                    throwable.message,
-                    throwable
-                )
-                errorLoadingView.visibility=View.VISIBLE;
-                loadingView.visibility = View.GONE
+                override fun onFailure(call: Call<ContributorListModel>?, throwable: Throwable) {
+                    Log.d(
+                        Constant.TAG(ContributorsFragment::class.java).toString(),
+                        throwable.message,
+                        throwable
+                    )
+                    errorLoadingView.visibility=View.VISIBLE;
+                    loadingView.visibility = View.GONE
+                }
+            })
+        } else{
+            val dialog = AlertDialog.Builder(context)
+            dialog.setTitle("Error")
+            dialog.setMessage("Internet Connection Not Found")
+            dialog.setPositiveButton("Open Settings") { text, listener ->
+                val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(settingsIntent)
+                ActivityCompat.finishAffinity(requireActivity())
             }
-        })
+            dialog.setNegativeButton("Exit App") { text, listener ->
+                ActivityCompat.finishAffinity(requireActivity())
+            }
+            dialog.create()
+            dialog.show()
+        }
+
     }
 
     private fun setUpRecyclerView(body: ContributorListModel) {
